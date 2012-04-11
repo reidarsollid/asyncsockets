92ecac1f2c3c5235c901ae66de69de80b36c9a68
Reidar Sollid <rsollid@gmail.com>
2012-04-11 14:27:31 +0200

    Using decoder to covert bytebuffer to charbuffer

--- src/no/bouvet/async/Client.java
+++ src/no/bouvet/async/Client.java
@@ -3,8 +3,10 @@ package no.bouvet.async;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 /**
  * User: Reidar Sollid
@@ -16,15 +18,16 @@ public class Client {
         try {
             AsynchronousSocketChannel socketChannel = AsynchronousSocketChannel.open();
             InetAddress address = InetAddress.getByName("localhost");
-            InetSocketAddress inetSocketAddress = new InetSocketAddress(address, 202000);
+            InetSocketAddress inetSocketAddress = new InetSocketAddress(address, 20200);
             socketChannel.connect(inetSocketAddress).get(); // Blocking
-
+            Future<Integer> write = socketChannel.write(ByteBuffer.wrap("Hello NIO2".getBytes("ISO-8859-1")));
+            System.out.println(write.get());
         } catch (IOException | ExecutionException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
     public static void main(String[] args) {
-
+        new Client();
     }
 }
--- src/no/bouvet/async/Server.java
+++ src/no/bouvet/async/Server.java
@@ -1,11 +1,16 @@
 package no.bouvet.async;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.charset.CharacterCodingException;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
 
 /**
  * User: Reidar Sollid
@@ -13,19 +18,36 @@ import java.util.concurrent.TimeUnit;
  * Time: 9:02 AM
  */
 public class Server {
+    private CharsetDecoder decoder;
+
     public Server() {
+        Charset charset = Charset.forName("ISO-8859-1");
+        decoder = charset.newDecoder();
         try {
-            AsynchronousServerSocketChannel serverSocketChannel = AsynchronousServerSocketChannel.open().bind(null); // null binds a free port
+            InetSocketAddress address = new InetSocketAddress("localhost", 20200);
+            AsynchronousServerSocketChannel serverSocketChannel = AsynchronousServerSocketChannel.open().bind(address); // null binds a free port
             Future<AsynchronousSocketChannel> channelFuture = serverSocketChannel.accept();
             AsynchronousSocketChannel socketChannel = channelFuture.get(); // Blocking
-            // //channelFuture.get(10, TimeUnit.SECONDS);
-
+            clientHandler(socketChannel);
         } catch (IOException | InterruptedException | ExecutionException e) {
             e.printStackTrace();
         }
     }
 
-    public static void main(String[] args) {
+    private void clientHandler(AsynchronousSocketChannel socketChannel) {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(256);
+        try {
+            socketChannel.read(byteBuffer).get();
+            System.out.println("Something is gotten");
+            byteBuffer.flip();
+            CharBuffer answer = decoder.decode(byteBuffer);
+            System.out.println(answer.toString());
+        } catch (InterruptedException | ExecutionException | CharacterCodingException e) {
+            e.printStackTrace();
+        }
+    }
 
+    public static void main(String[] args) {
+        new Server();
     }
 }
